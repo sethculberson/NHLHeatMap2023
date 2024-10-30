520e2ef16a74eedffe0513aeea9505516cb64c8a
sethculberson <sethculb@bu.edu>
2024-10-30 17:13:09 -0400

    Removed plot setup code from plot_shots_by_name function

--- bruins_heat_map.py
+++ bruins_heat_map.py
@@ -10,23 +10,23 @@ df = pd.read_csv('data/shots_2023.csv')
 #Selecting which statistics to analyze out of the data set
 data_to_track = ["shooterName","xGoal","goal","xCordAdjusted","yCordAdjusted","shotOnEmptyNet","shotDistance"]
 
+#cleaning the data
+filtered_df = df[data_to_track]
+filtered_df = filtered_df[filtered_df["shotOnEmptyNet"]==0] #not tracking empty net shots
+filtered_df = filtered_df[filtered_df["shotDistance"] <= 89]
+filtered_df = filtered_df[filtered_df["xCordAdjusted"] <= 89]
+
+#set up rink and fig, axes
+rink = hockey_rink.Rink(rotation=90,net={"visible": False}) #rotating the rink vertically
+fig, axes = plt.subplots(1,1,figsize=(6,8))
+
 #In the future, the name will be input by the user, and the heat map will be generated
 def plot_shots_by_name(text):
-    rink = hockey_rink.Rink(rotation=90,net={"visible": False}) #rotating the rink vertically
-    
     player_name = text
+    playerspecific_df = filtered_df[filtered_df["shooterName"]==player_name]
     
-    #cleaning the data
-    filtered_df = df[data_to_track]
-    filtered_df = filtered_df[filtered_df["shotOnEmptyNet"]==0] #not tracking empty net shots
-    filtered_df = filtered_df[filtered_df["shotDistance"] <= 89]
-    filtered_df = filtered_df[filtered_df["xCordAdjusted"] <= 89]
-    filtered_df = filtered_df[filtered_df["shooterName"]==player_name]
-    
-    goals = filtered_df[filtered_df["goal"]==1]
-    misses = filtered_df[filtered_df["goal"]==0]
-
-    fig, axes = plt.subplots(1,1,figsize=(6,8))
+    goals = playerspecific_df[playerspecific_df["goal"]==1]
+    misses = playerspecific_df[playerspecific_df["goal"]==0]
 
     #create scatter plots for makes/misses, with the point size being weighted by the expected goal probability for the shot
     rink.scatter(x=goals["xCordAdjusted"], y=goals["yCordAdjusted"], color="green",alpha=0.8,s=goals["xGoal"]*240+20, ax=axes,plot_range="ozone",draw_kw={"display_range": "ozone"})
